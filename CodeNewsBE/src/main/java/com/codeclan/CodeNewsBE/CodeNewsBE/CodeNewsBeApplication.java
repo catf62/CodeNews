5430986c2e26ceddd8b8967ed88b7e920561a675
Rob Wilson <rob.wilson195@gmail.com>
2019-02-01 15:45:53 +0000

    Initial setup.

---
+++ CodeNewsBE/src/main/java/com/codeclan/CodeNewsBE/CodeNewsBE/CodeNewsBeApplication.java
@@ -0,0 +1,14 @@
+package com.codeclan.CodeNewsBE.CodeNewsBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNewsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNewsBeApplication.class, args);
+	}
+
+}
+
