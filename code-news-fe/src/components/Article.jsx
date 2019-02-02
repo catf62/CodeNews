cb1992b77eac127ba65aebae2bb401480a2226b0
Catriona Ford <catriona.b.ford@gmail.com>
2019-02-02 13:29:14 +0000

    Article.jsx started

---
+++ code-news-fe/src/components/Article.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Article extends Component {
+  render() {
+    return (
+      // <li className='article'>
+      //   <a href={this.props.movie.url}>{this.props.movie.name}</a>
+      // </li>
+    );
+  }
+}
+
+export default MovieItem;
