77983ffb73d92a5733751936927e11f6a78ab249
Graeme Congreve <junkmailgmc@gmail.com>
2019-02-02 11:41:56 +0000

    finished Author a model

---
+++ CodeNewsBE/src/main/java/com/codeclan/CodeNewsBE/CodeNewsBE/models/Article.java
@@ -0,0 +1,4 @@
+package com.codeclan.CodeNewsBE.CodeNewsBE.models;
+
+public class Article {
+}
--- CodeNewsBE/src/main/java/com/codeclan/CodeNewsBE/CodeNewsBE/models/Author.java
+++ CodeNewsBE/src/main/java/com/codeclan/CodeNewsBE/CodeNewsBE/models/Author.java
@@ -1,6 +1,10 @@
 package com.codeclan.CodeNewsBE.CodeNewsBE.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "authors")
@@ -10,4 +14,49 @@ public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonIgnoreProperties("author")
+    @OneToMany(mappedBy = "author")
+    private List<Article> articles;
+
+    public Author(String name) {
+        this.name = name;
+        this.articles = new ArrayList<>();
+
+    }
+
+    public Author() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public void addArticle(Article article){
+        this.articles.add(article);
+    }
 }
