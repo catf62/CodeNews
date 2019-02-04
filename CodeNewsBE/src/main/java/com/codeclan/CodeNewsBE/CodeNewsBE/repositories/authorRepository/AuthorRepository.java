9c442c125b8afb5cc3592f559a31c7e6882c9b27
Rob Wilson <rob.wilson195@gmail.com>
2019-02-04 09:44:10 +0000

    Projections working. Can see the sub-article or sub-author on relevant requests.

--- CodeNewsBE/src/main/java/com/codeclan/CodeNewsBE/CodeNewsBE/repositories/authorRepository/AuthorRepository.java
+++ CodeNewsBE/src/main/java/com/codeclan/CodeNewsBE/CodeNewsBE/repositories/authorRepository/AuthorRepository.java
@@ -1,11 +1,11 @@
 package com.codeclan.CodeNewsBE.CodeNewsBE.repositories.authorRepository;
 
 import com.codeclan.CodeNewsBE.CodeNewsBE.models.Author;
-import com.codeclan.CodeNewsBE.CodeNewsBE.projections.EmbedAuthor;
+import com.codeclan.CodeNewsBE.CodeNewsBE.projections.EmbedArticles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(excerptProjection = EmbedAuthor.class)
+@RepositoryRestResource(excerptProjection = EmbedArticles.class)
 public interface AuthorRepository extends AuthorRepositoryCustom, JpaRepository<Author, Long> {
 }
