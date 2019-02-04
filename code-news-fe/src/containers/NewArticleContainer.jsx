35f28f2fd4b4f3ac503a17a21b5d8f92bf73f87e
Christopher Milligan <milliganjchristopher@gmail.com>
2019-02-04 14:57:02 +0000

    new article form added

--- code-news-fe/src/containers/NewArticleContainer.jsx
+++ code-news-fe/src/containers/NewArticleContainer.jsx
@@ -16,6 +16,16 @@ class NewArticleContainer extends Component {
   render(){
     return (
       <form>
+        <label htmlFor="Headline">Headline</label>
+        <input type="text" value={this.state.headline} id="Headline"/>
+        <label htmlFor="Author">Author</label>
+        <input type="text" value={this.state.author} id="Author"/>
+        <label htmlFor="Content">Content</label>
+        <input type="text" value={this.state.content} id="Content"/>
+        <label htmlFor="Image url">Image url</label>
+        <input type="text" value={this.state.imageurl} id="Image url"/>
+        <label htmlFor="Keywords">Keywords</label>
+        <input type="text" value={this.state.keywords} id="Keywords"/>
         <input type="submit" value="Save"/>
       </form>
     )
