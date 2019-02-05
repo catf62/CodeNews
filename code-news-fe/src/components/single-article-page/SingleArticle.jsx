fee0e6ab47744791d711416eb4636b41cd809e38
Catriona Ford <catriona.b.ford@gmail.com>
2019-02-05 11:50:32 +0000

    date displaying correctly on single article page

--- code-news-fe/src/components/single-article-page/SingleArticle.jsx
+++ code-news-fe/src/components/single-article-page/SingleArticle.jsx
@@ -15,13 +15,23 @@ class SingleArticle extends Component {
       return <p key={index} className="Medium-Blue-BG Solid-Black-Border-Curved Single-Article-Page-Keyword">{keyword.word}</p>
     })
 
+    let day = this.props.article.datePosted.getDate().toString();
+    if (this.props.article.datePosted.getDate() < 10) {
+      day = ("0" + day).slice(-2);
+    }
+
+    let month = (this.props.article.datePosted.getMonth() + 1).toString();
+    if (this.props.article.datePosted.getMonth() < 10) {
+      month = ("0" + month).slice(-2);
+    }
+
   return (
       <div className="Solid-Black-Border-Curved Single-Article-Page-Article">
         <h3 className="Single-Article-Page-Article-Headline">{this.state.article.headline}</h3>
         <h5 className="Single-Article-Page-Article-Author">By {this.state.article.author.name}</h5>
         <p className="Single-Article-Page-Article-Content">{this.state.article.content}</p>
         <img className="Single-Article-Page-Article-Image" src={this.state.article.imageUrl}/>
-        <p>{this.state.article.datePosted.getDate()}/{this.state.article.datePosted.getMonth()}/{this.state.article.datePosted.getFullYear()}</p>
+        <p>{day}/{month}/{this.state.article.datePosted.getFullYear()}</p>
         <div className="Single-Article-Page-Article-Keywords">
           {displayedKeywords}
         </div>
