d3d44d3fee3fb807ff3435c79a7554d972cf3e7d
Catriona Ford <catriona.b.ford@gmail.com>
2019-02-05 10:12:34 +0000

    all fields capturing data in new article form

--- code-news-fe/src/containers/NewArticleContainer.jsx
+++ code-news-fe/src/containers/NewArticleContainer.jsx
@@ -18,12 +18,44 @@ class NewArticleContainer extends Component {
     }
 
     this.headlineKeyUp = this.headlineKeyUp.bind(this);
+    this.dateOnChange = this.dateOnChange.bind(this);
+    this.authorKeyUp = this.authorKeyUp.bind(this);
+    this.contentKeyUp = this.contentKeyUp.bind(this);
+    this.imageurlKeyUp = this.imageurlKeyUp.bind(this);
   }
 
   headlineKeyUp(event) {
     console.log(this.state);
   this.setState({
     headline: event.target.value
+  });
+  }
+
+  dateOnChange(event) {
+    const capturedDate = event.target.value;
+    this.setState({
+      date: capturedDate.slice(8, 10) + "/" + capturedDate.slice(5, 7) + "/" + capturedDate.slice(0, 4)
+    });
+    }
+
+  authorKeyUp(event) {
+    console.log(this.state);
+  this.setState({
+    author: event.target.value
+  });
+  }
+
+  contentKeyUp(event) {
+    console.log(this.state);
+  this.setState({
+    content: event.target.value
+  });
+  }
+
+  imageurlKeyUp(event) {
+    console.log(this.state);
+  this.setState({
+    imageurl: event.target.value
   });
   }
 
@@ -37,13 +69,13 @@ class NewArticleContainer extends Component {
           <label htmlFor="Headline">Headline</label>
           <input onKeyUp={this.headlineKeyUp} type="text" id="Headline"/>
           <label htmlFor="Date">Date</label>
-          <input type="date" id="Date"/>
+          <input onChange={this.dateOnChange} type="date" id="Date"/>
           <label htmlFor="Author">Author</label>
-          <input type="text" id="Author"/>
+          <input onKeyUp={this.authorKeyUp} type="text" id="Author"/>
           <label htmlFor="Content">Content</label>
-          <input type="text" id="Content"/>
+          <input onKeyUp={this.contentKeyUp}  type="text" id="Content"/>
           <label htmlFor="Image url">Image url</label>
-          <input type="text" id="Image url"/>
+          <input onKeyUp={this.imageurlKeyUp} type="text" id="Image url"/>
           <label htmlFor="Keywords">Keywords</label>
           <input type="text" id="Keywords"/>
           <input type="submit" value="Save"/>
