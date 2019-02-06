88ca843d42fa8d52ff00ee0738986ac11b9b7e27
Catriona Ford <catriona.b.ford@gmail.com>
2019-02-06 09:48:42 +0000

    new author form fleshed out

--- code-news-fe/src/containers/EditAuthorContainer.jsx
+++ code-news-fe/src/containers/EditAuthorContainer.jsx
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import Header from '../components/general/Header';
+import NavBar from '../components/general/NavBar';
+import Footer from '../components/general/Footer';
 
 class EditAuthorContainer {
   constructor(props) {
--- code-news-fe/src/containers/NewAuthorContainer.jsx
+++ code-news-fe/src/containers/NewAuthorContainer.jsx
@@ -9,9 +9,15 @@ class NewAuthorContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: ""
+      name: "",
+      position:"",
+      bio:"",
+      imageUrl: ""
     }
     this.nameKeyUp = this.nameKeyUp.bind(this);
+    this.positionKeyUp = this.positionKeyUp.bind(this);
+    this.bioKeyUp = this.bioKeyUp.bind(this);
+    this.imageUrlKeyUp = this.imageUrlKeyUp.bind(this);
     this.handleAuthorSelect = this.handleAuthorSelect.bind(this);
   }
 
@@ -19,6 +25,27 @@ class NewAuthorContainer extends Component {
     console.log(this.state);
   this.setState({
     name: event.target.value
+  });
+  }
+
+  positionKeyUp(event) {
+    console.log(this.state);
+  this.setState({
+    position: event.target.value
+  });
+  }
+
+  bioKeyUp(event) {
+    console.log(this.state);
+  this.setState({
+    bio: event.target.value
+  });
+  }
+
+  imageUrlKeyUp(event) {
+    console.log(this.state);
+  this.setState({
+    imageUrl: event.target.value
   });
   }
 
@@ -35,6 +62,16 @@ class NewAuthorContainer extends Component {
         <form>
           <label htmlFor="Name">Author Name</label>
           <input onKeyUp={this.nameKeyUp} type="text" id="Name"/>
+
+          <label htmlFor="Position">Position</label>
+          <input onKeyUp={this.positionKeyUp} type="text" id="Position"/>
+
+          <label htmlFor="Bio">Bio</label>
+          <input onKeyUp={this.bioKeyUp} type="text" id="Bio"/>
+
+          <label htmlFor="ImageUrl">Image url</label>
+          <input onKeyUp={this.imageUrlKeyUp} type="text" id="ImageUrl"/>  
+
           <input type="submit" value="Save"/>
         </form>
       </div>
