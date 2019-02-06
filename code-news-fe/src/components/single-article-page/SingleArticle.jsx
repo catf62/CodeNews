070e4ab220aab40e8e46980c5ff809bc5f227c10
Catriona Ford <catriona.b.ford@gmail.com>
2019-02-06 21:01:18 +0000

    single page article css complete

--- code-news-fe/src/components/single-article-page/SingleArticle.jsx
+++ code-news-fe/src/components/single-article-page/SingleArticle.jsx
@@ -12,7 +12,7 @@ class SingleArticle extends Component {
   render(){
     const displayedKeywords =
      this.state.article.keywords.map((keyword, index) => {
-      return <p key={index} className="Medium-Blue-BG Solid-Black-Border-Curved Single-Article-Page-Keyword">{keyword.word}</p>
+      return <p key={index} className="Single-Article-Page-Keyword-Box">{keyword.word}</p>
     })
 
     let day = this.props.article.datePosted.getDate().toString();
@@ -26,21 +26,33 @@ class SingleArticle extends Component {
     }
 
   return (
-      <div className="Solid-Black-Border-Curved Single-Article-Page-Article">
-        <h3 className="Single-Article-Page-Article-Headline">{this.state.article.headline}</h3>
-        <h5 className="Single-Article-Page-Article-Author">By {this.state.article.author.name}</h5>
-        <p className="Single-Article-Page-Article-Content">{this.state.article.content}</p>
-        <img className="Single-Article-Page-Article-Image" src={this.state.article.imageUrl}/>
-        <p>{day}/{month}/{this.state.article.datePosted.getFullYear()}</p>
-        <div className="Single-Article-Page-Article-Keywords">
-          {displayedKeywords}
+      <div className="Single-Article-Page-Article">
+        <div className="Single-Article-Box">
+
+          <div className="Single-Article-Headline-And-Image-Box">
+            <h3 className="Single-Article-Page-Article-Headline">{this.state.article.headline}</h3>
+            <img className="Single-Article-Page-Article-Image" src={this.state.article.imageUrl}/>
+          </div>
+
+          <div className="Single-Article-Page-Author-And-Date-Box">
+            <h5 className="Single-Article-Page-Article-Author">By {this.state.article.author.name}</h5>
+            <p className="Single-Article-Page-Article-Date">{day}/{month}/{this.state.article.datePosted.getFullYear()}</p>
+          </div>
+          <br></br>
+
+          <p className="Single-Article-Page-Article-Content">{this.state.article.content}</p>
+          <br></br>
+          <div className="Single-Article-Page-Article-Keywords">
+            <p>Keywords: </p>
+            {displayedKeywords}
+          </div>
+          <Link to={'/article/'+this.state.article.id+'/edit'}>
+          <button className="Single-Article-Page-Button">Edit</button>
+          </Link>
+          <Link to={'/article/'+this.state.article.id+'/delete'}>
+          <button className="Single-Article-Page-Button">Delete</button>
+          </Link>
         </div>
-        <Link to={'/article/'+this.state.article.id+'/edit'}>
-        <button className="Button">Edit</button>
-        </Link>
-        <Link to={'/article/'+this.state.article.id+'/delete'}>
-        <button className="Button">Delete</button>
-        </Link>
       </div>
     )
   }
