e1c0cdcae6e48e01357871a8a9509da5e6d98a12
Catriona Ford <catriona.b.ford@gmail.com>
2019-02-06 15:10:45 +0000

    most of article list styling complete

--- code-news-fe/src/components/landing-page/ArticleLink.jsx
+++ code-news-fe/src/components/landing-page/ArticleLink.jsx
@@ -32,9 +32,14 @@ class ArticleLink extends Component {
     return (
       <div className="Solid-Black-Border-Curved Landing-Page-Article" id={this.state.id}>
       <h3 className="Landing-Page-Article-Headline">{this.props.article.headline}</h3>
-      <h5 className="Landing-Page-Article-Author">By {this.props.article.author.name}</h5>
+
+      <div className="Landing-Page-Date-Author-Flex">
+        <h5 className="Landing-Page-Article-Author ">By {this.props.article.author.name}</h5>
+        <p className="Landing-Page-Article-Date ">{day}/{month}/{this.props.article.datePosted.getFullYear()}</p>
+      </div>
+
       <p className="Landing-Page-Article-Content">{this.props.article.content.slice(0, 150) + "..."}</p>
-      <p>{day}/{month}/{this.props.article.datePosted.getFullYear()}</p>
+
       <div className="Landing-Page-Article-Keywords">
       {keywords}
       </div>
